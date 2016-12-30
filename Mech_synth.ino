01e63f5bed4cb1dd03edf9c26c91cd89722e26b5
Sanna <luinelen@iki.fi>
2016-12-30 13:38:47 +0200

    Lisätty leditikku, yhdeksäs nappi ja potti

--- Mech_synth.ino
+++ Mech_synth.ino
@@ -1,3 +1,5 @@
+#include <Adafruit_NeoPixel.h>
+
 int timeOn = 100;
 int timeOff = 500;
 
@@ -23,6 +25,18 @@ int dataPin         = 4; // Connects to the Q7 pin the 165
 int clockPin        = 3; // Connects to the Clock pin the 165
 
 unsigned int pinValues;
+
+#define PIXEL_PIN 1
+
+#define BUTTON_PIN 0
+
+#define POT_PIN 0
+
+int activeStep = 0;
+
+int currentStep = 0;
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(8, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
 
 /* This function is essentially a "shift-in" routine reading the
  * serial Data from the shift register chips and representing
@@ -83,10 +97,29 @@ void setup() {
   pinMode(8, OUTPUT);
   pinMode(7, OUTPUT);
   pinMode(6, OUTPUT);
+
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
 }
 
 // the loop function runs over and over again forever
 void loop() {
+  currentStep++;
+  if(currentStep>7)
+    currentStep=0;
+
+  timeOff = analogRead(POT_PIN);
+
+  if(digitalRead(BUTTON_PIN) == LOW)
+  {
+    activeStep++;
+
+    if(activeStep>8)
+      activeStep=0;
+  }
+  
   /* Read the state of all zones.
   */
   pinValues = read_shift_regs();
@@ -94,11 +127,31 @@ void loop() {
   for(int i=0; i<DATA_WIDTH; i++)
   {
     if((pinValues >> i) & 1)
+    {
       digitalWrite(i+6, HIGH);
+      if(i==activeStep)
+        strip.setPixelColor(7-i, strip.Color(15,15,0));//yellow
+      else
+        strip.setPixelColor(7-i, strip.Color(15,0,0));//red
+    }
+    else if(i==activeStep)
+      strip.setPixelColor(7-i, strip.Color(0,15,0));//green
+    else if(i==currentStep)
+      strip.setPixelColor(7-i, strip.Color(0,0,15));//blue
   }
+  strip.show();
 
   delay(timeOn);
 
   for(int i=0; i<DATA_WIDTH; i++)
+  {
     digitalWrite(i+6, LOW);
+    if(i==activeStep)
+      strip.setPixelColor(7-i, strip.Color(0,15,0));//green
+    else
+      strip.setPixelColor(7-i, strip.Color(0,0,0));
+  }
+  strip.show();
+
+  delay(timeOff);
 }
